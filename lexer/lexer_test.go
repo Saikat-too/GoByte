f7829daf134f596e8b19f06cb6463638f03b0421
Saikat-too <saikatsingha00q@gmail.com>
2024-09-27 20:26:02 +0600

    Enable to interprate the identifier and number

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,20 +6,31 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := "=+*(),"
+	input := `five = 5
+    
+  ten = 10 
+
+  add = five+ten 
+
+
+
+  `
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "five"},
 		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.MUL, "*"},
-		{token.DIV, "/"},
-		{token.COMMA, ","},
-		{token.MINUS, "-"},
+		{token.IDENT, "ten"},
 	}
 
 	l := New(input)
