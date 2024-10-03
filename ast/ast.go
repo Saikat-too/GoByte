33bb8e8a10e0894a59995a31b5be2ce8c3ecbf51
Saikat-too <saikatsingha00q@gmail.com>
2024-10-03 14:19:29 +0600

    Implementing inital ast of parser

---
+++ ast/ast.go
@@ -0,0 +1,49 @@
+package ast
+
+import "gobyte/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return " "
+	}
+}
+
+type AssignStatement struct {
+	Name  *Identifier
+	Token token.Token
+	Value Expression
+}
+
+func (as *AssignStatement) statementNode() {}
+
+func (as *AssignStatement) TokenLiteral() string {
+	return as.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
